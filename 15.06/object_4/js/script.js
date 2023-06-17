e59fb9b716b99ecd42c5cf018a83fd07b96416ab
velthereal <lera2005olex@gmail.com>
2023-06-18 00:38:53 +0300

    finished homework 15.06

--- 15.06/object_4/js/script.js
+++ 15.06/object_4/js/script.js
@@ -4,4 +4,31 @@ let minutes = document.querySelector('#minutes span');
 let seconds = document.querySelector('#seconds span');
 
 let currentDate = new Date();
-console.log(currentDate)
+
+let nextYear = currentDate.getFullYear() + 1;
+let newYearDate = new Date(nextYear, 0, 1);
+console.log(newYearDate)
+let difference = newYearDate.getTime() - currentDate.getTime();
+console.log(difference)
+
+let daysTime = Math.floor(difference / (1000 * 60 * 60 * 24));
+let hoursTime = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+let minutesTime = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+let secondsTime = Math.floor((difference % (1000 * 60)) / 1000);
+
+if(daysTime < 10){
+	daysTime = '0' + daysTime;
+}
+if(hoursTime < 10){
+	hoursTime = '0' + hoursTime;
+}
+if(minutesTime < 10){
+	minutesTime = '0' + minutesTime;
+}
+if(secondsTime < 10){
+	secondsTime = '0' + secondsTime;
+}
+days.textContent = daysTime;
+hours.textContent = hoursTime;
+minutes.textContent = minutesTime;
+seconds.textContent = secondsTime;
