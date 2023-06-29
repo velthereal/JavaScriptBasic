b56ac33f5f50ab8326dd1defde84c12b46a3e965
velthereal <lera2005olex@gmail.com>
2023-06-29 12:59:40 +0300

    homeworks

--- 27.06/form_5/js/script.js
+++ 27.06/form_5/js/script.js
@@ -2,15 +2,15 @@ let form = document.forms[0];
 let elements = form.elements;
 
 form.addEventListener('submit', (e) => {
-	// for(let i = 0; i < elements.length - 1; i++){
-	// 	if(elements[i].value.length == 0){
-	// 		elements[i].classList.add('error');
-	// 		e.preventDefault();
-	// 	}
-	// 	elements[i].addEventListener('input', (e) => {
-	// 		elements[i].classList.remove('error');
-	// 	})
-	// }
+	for(let i = 0; i < elements.length - 1; i++){
+		if(elements[i].value.length < 3){
+			elements[i].classList.add('error');
+			e.preventDefault();
+		}
+		elements[i].addEventListener('input', (e) => {
+			elements[i].classList.remove('error');
+		})
+	}
 
 	if(!checkEmail(form.email.value)){
 		form.email.classList.add('error');
@@ -18,16 +18,30 @@ form.addEventListener('submit', (e) => {
 	} else {
 		form.email.classList.add('right');
 	}
-	// console.log(checkEmail(form.email.value));
-});
-form.email.addEventListener('input', (e) => {
-	if(checkEmail(form.email.value)){
-	form.email.classList.add('right');
+
+	if(!checkPassword(form.password.value)){
+		form.password.classList.add('error');
+		e.preventDefault();
 	} else {
-		form.email.classList.add('error');
+		form.email.classList.add('right');
 	}
-})
+
+});
+
+// form.email.addEventListener('input', (e) => {
+// 	if(checkEmail(form.email.value)){
+// 	form.email.classList.add('right');
+// 	} else {
+// 		form.email.classList.add('error');
+// 	}
+// })
+
 function checkEmail(email){
 	let temp = /(\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,6})/;
 	return temp.test(email);
+}
+
+function checkPassword(pass){
+	let temp = /[A-Za-z0-9]/;
+	return temp.test(pass);
 }
